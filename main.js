bedd2d21b223f43a9e03510223973aa0d4afe435
Swaraj2004 <swarajchavan2004@gmail.com>
2024-08-25 10:19:01 +0530

    Add mobile view error

--- main.js
+++ main.js
@@ -4,12 +4,24 @@ import { displayPokemons, displayByType } from "./display.js";
 import { addRemove, deleteItem, buy } from "./cart.js";
 import { searchPokemon } from "./search.js";
 
+function checkScreenWidth() {
+  if (window.innerWidth < 1200) {
+    document.getElementById("container").style.display = "none";
+    document.getElementById("view-error").style.display = "flex";
+  } else {
+    assignColors();
+    document.getElementById("container").style.display = "flex";
+    document.getElementById("view-error").style.display = "none";
+  }
+}
+
 async function main() {
   document.querySelector("#search-bar input").value = "";
   window.onload = function () {
-    assignColors();
-    document.getElementById("container").style.display = "flex";
+    checkScreenWidth();
+    window.addEventListener("resize", checkScreenWidth);
   };
+
   const pokemon = await getPokemons();
   // console.log(pokemon);
   displayPokemons(pokemon);
